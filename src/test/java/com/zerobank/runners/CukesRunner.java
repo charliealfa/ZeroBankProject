bd909eaf39eaec0b22c173cd787402eacea88c80
Cihan ASLAN <aslan2341@gmail.com>
2022-02-16 00:30:23 +0300

    rerun added

--- src/test/java/com/zerobank/runners/CukesRunner.java
+++ src/test/java/com/zerobank/runners/CukesRunner.java
@@ -11,8 +11,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {"json:target/cucumber.json",
-                "html:target/default-html-reports",
-                "rerun:target/rerun.txt"},
+                "html:target/default-html-reports"},
         features = "src/test/resources/features",
         glue = "com/zerobank/stepdefinitions",
         dryRun = false,
